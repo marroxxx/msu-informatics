5c5d84427bbbdd1779feb26175a3c370a05a26ff
marroxxx <158850232+marroxxx@users.noreply.github.com>
2024-11-02 15:28:31 +0300

    Add complex

---
+++ project/Complex.cpp
@@ -0,0 +1,72 @@
+#include "Complex.hpp"
+
+Complex::Complex(double a, double b) {
+    this->re = a;
+    this->im = b;
+}
+
+Complex::Complex(const Complex &x) {
+    this->re = x.re;
+    this->im = x.im;
+}
+
+Complex 
+Complex::operator+(const Complex &x) const {
+    Complex res;
+    res.re = this->re + x.re;
+    res.im = this->im + x.im;
+    return res;
+}
+
+Complex 
+Complex::operator-(const Complex &x) const {
+    Complex res;
+    res.re = this->re - x.re;
+    res.im = this->im - x.im;
+    return res;
+}
+
+Complex 
+Complex::operator*(const Complex &x) const {
+    Complex res;
+    res.re = this->re * x.re - this->im * x.im;
+    res.im = this->im * x.re + this->re * x.im;
+    return res;
+}
+
+Complex
+Complex::operator/(const Complex &x) const {
+    Complex res;
+    res.re = (this->re * x.re + this->im * x.im) / (x.re * x.re + x.im * x.im);
+    res.im = (this->im * x.re - this->re * x.im) / (x.re * x.re + x.im * x.im);
+    return res;
+}
+
+bool 
+Complex::operator==(const Complex &x) const {
+    return this->re == x.re && this->im == x.im;
+}
+
+bool 
+Complex::operator!=(const Complex &x) const {
+    return !(*this == x);
+}
+
+std::ostream &
+operator<<(std::ostream &out, const Complex &x) {
+    std::cout << x.re;
+    if (x.im >= 0) {
+        std::cout << " + " << x.im << "i";
+    } else {
+        std::cout << " - " << -x.im << "i";
+    }
+    return out;
+}
+
+int 
+main(void) {
+    Complex x(INT_MAX, 2);
+    Complex y(1, 4);
+    std::cout << y / x << std::endl;
+    return 0;
+}
---
+++ project/Complex.hpp
@@ -0,0 +1,25 @@
+#ifndef COMPLEX_HPP
+#define COMPLEX_HPP
+
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+class Complex
+{
+private:
+    double re;
+    double im;
+public:
+    Complex(double a = 0, double b = 0);
+    Complex(const Complex &x);
+    Complex operator+(const Complex &x) const;
+    Complex operator-(const Complex &x) const;
+    Complex operator*(const Complex &x) const;
+    Complex operator/(const Complex &x) const;
+    bool operator==(const Complex &x) const;
+    bool operator!=(const Complex &x) const;
+    friend std::ostream &operator<<(std::ostream &out, const Complex &x);
+};
+
+#endif
